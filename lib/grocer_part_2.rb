91db79354af095851431d1d71293cd5e4097ffbd
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-07 02:29:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,7 +8,7 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_in_collection(coupons[count][:item], cart)
     coupon_item_name = "#{coupons[count][:item]} W/COUPON"
     cart_item_with_coup = find_item_by_name_in_collection(coupon_item_name, cart)
-    if cart_item && cart_item[:count] >= 
+    if cart_item && cart_item[:count] >= coupons[count]
       
     end
     counter += 1
